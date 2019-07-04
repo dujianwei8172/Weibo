c341503e268ea1a69c5e4db5b0cb07c5709f69b0
Mashiro <mashiro@MashirotekiMacBook-Air.local>
2019-07-04 17:54:50 +0800

    1231

--- 234.py
+++ 234.py
@@ -1,70 +1,72 @@
-learning_rate = 0.1
-state_size = 128 # hidden layer num of features
-n_classes = 19
-n_features = 23
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import tensorflow.contrib.rnn as rnn
+import matplotlib.pyplot as plt
 
-#输入
-x = tf.placeholder(tf.float32, [None, None, n_features], name='input_placeholder')  #batch_size, time_step, feat_len
-y = tf.placeholder(tf.float32, [None, None, n_classes], name='labels_placeholder')  #batch_size, time_step, n_classes
+TIME_STEPS=28
+BATCH_SIZE=128
+HIDDEN_UNITS1=30
+HIDDEN_UNITS=10
+LEARNING_RATE=0.001
+EPOCH=50
 
-batch_size = tf.placeholder(tf.int32, (), name='batch_size')
-time_steps = tf.placeholder(tf.int32, (), name='times_step')
 
-#双向rnn
-lstm_fw_cell = tf.contrib.rnn.BasicLSTMCell(state_size)
-lstm_bw_cell = tf.contrib.rnn.BasicLSTMCell(state_size)
+TRAIN_EXAMPLES=42000
+TEST_EXAMPLES=28000
 
-init_fw = lstm_fw_cell.zero_state(batch_size, dtype=tf.float32)
-init_bw = lstm_bw_cell.zero_state(batch_size, dtype=tf.float32)
 
-weights = tf.get_variable("weights", [2 * state_size, n_classes], dtype=tf.float32,   #注意这里的维度
-                         initializer = tf.random_normal_initializer(mean=0, stddev=1))
-biases = tf.get_variable("biases", [n_classes], dtype=tf.float32, 
-                        initializer = tf.random_normal_initializer(mean=0, stddev=1))
+#------------------------------------Generate Data-----------------------------------------------#
+#generate data
 
-outputs, final_states = tf.nn.bidirectional_dynamic_rnn(lstm_fw_cell,
-                                                       lstm_bw_cell,
-                                                       x,
-                                                       initial_state_fw = init_fw,
-                                                       initial_state_bw = init_bw)
+train_frame = pd.read_csv("../Mnist/train.csv") 
+test_frame = pd.read_csv("../Mnist/test.csv") # pop the labels and one-hot
+train_labels_frame = train_frame.pop("label")
 
-outputs = tf.concat(outputs, 2)   #将前向和后向的状态连接起来
-state_out = tf.matmul(tf.reshape(outputs, [-1, 2 * state_size]), weights) + biases  #注意这里的维度
-logits = tf.reshape(state_out, [batch_size, time_steps, n_classes])
+X_train = train_frame.astype(np.float32).values 
+y_train=pd.get_dummies(data=train_labels_frame).values 
+X_test = test_frame.astype(np.float32).values
 
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            logits=logits, labels=y))  #计算交叉熵
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)  #优化方法
-train_op = optimizer.minimize(loss_op)
 
-#进行softmax计算
-probs = tf.nn.softmax(logits, -1)   #-1也是默认值，表示在最后一维进行运算
-predict = tf.argmax(probs, -1)#最大的概率在最后一维的哪一列，从0计数
-                                #维度变为  batch_size * time_step
+X_train=np.reshape(X_train,newshape=(-1,28,28))
+X_test=np.reshape(X_test,newshape=(-1,28,28))
 
-def train_network(num_epochs = 100):
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())  #初始化variable
-        
-        intervel = 5
-        
-        for epoch in range(num_epochs):
-            #开始训练
-            for idx, (time_step, inputs, labels, idxes) in enumerate(get_dump_seq_data(1)):
-                _= sess.run([train_op],
-                           feed_dict = {x: inputs,
-                                       y:labels,
-                                       batch_size:len(inputs),
-                                       time_steps: time_step})
-            print("epoch %d train done" % epoch)
-            #这一轮训练完毕，计算损失值和准确率
-            
-            if epoch % intervel == 0 and epoch > 1:
-                #训练集误差
-                acc_record, total_df, total_acc, loss = compute_accuracy(sess, 1)  #这里是我自定义的函数，与整个架构关系不大
-                #验证集误差
-                acc_record_dev, total_df_dev, total_acc_dev, loss_dev = compute_accuracy(sess, 0)
-                print("train_acc: %.6f, train_loss: %.6f; dev_acc: %.6f, dev_loss: %.6f" % (total_acc, loss, total_acc_dev, loss_dev))
-                print("- " * 50)
-                if num_epochs - epoch <= intervel:
-                    return acc_record, total_df, acc_record_dev, total_df_dev
+graph=tf.Graph()
+with graph.as_default():
+     X_p=tf.placeholder(dtype=tf.float32,shape=(None,TIME_STEPS,28),name="input_placeholder") 
+     y_p=tf.placeholder(dtype=tf.float32,shape=(None,10),name="pred_placeholder")
+     lstm_forward_1=rnn.BasicLSTMCell(num_units=HIDDEN_UNITS1) 
+     lstm_forward_2=rnn.BasicLSTMCell(num_units=HIDDEN_UNITS) 
+     lstm_forward=rnn.MultiRNNCell(cells=[lstm_forward_1,lstm_forward_2])
+     lstm_backward_1 = rnn.BasicLSTMCell(num_units=HIDDEN_UNITS1) 
+     lstm_backward_2 = rnn.BasicLSTMCell(num_units=HIDDEN_UNITS) 
+     lstm_backward=rnn.MultiRNNCell(cells=[lstm_backward_1,lstm_backward_2])
+     
+     outputs,states=tf.nn.bidirectional_dynamic_rnn( cell_fw=lstm_forward, cell_bw=lstm_backward, inputs=X_p, dtype=tf.float32 )
+     outputs_fw=outputs[0]
+     outputs_bw = outputs[1]
+     h=outputs_fw[:,-1,:]+outputs_bw[:,-1,:]
+     cross_loss=tf.losses.softmax_cross_entropy(onehot_labels=y_p,logits=h) #print(loss.shape)
+     correct_prediction = tf.equal(tf.argmax(h, 1), tf.argmax(y_p, 1))
+     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float")) 
+     optimizer=tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss=cross_loss) 
+     init=tf.global_variables_initializer()
+with tf.Session(graph=graph) as sess:
+    sess.run(init)
+    for epoch in range(1,EPOCH+1):
+         train_losses=[] 
+         accus=[] #test_losses=[] 
+         print("epoch:",epoch) 
+         for j in range(TRAIN_EXAMPLES//BATCH_SIZE):
+              _,train_loss,accu=sess.run( fetches=(optimizer,cross_loss,accuracy), 
+                                         feed_dict={ X_p:X_train[j*BATCH_SIZE:(j+1)*BATCH_SIZE],
+                                                    y_p:y_train[j*BATCH_SIZE:(j+1)*BATCH_SIZE] } )
+              train_losses.append(train_loss)
+              accus.append(accu)
+         print("average training loss:", sum(train_losses) / len(train_losses))
+         print("accuracy:",sum(accus)/len(accus))
+--------------------- 
+作者：luoganttcc 
+来源：CSDN 
+原文：https://blog.csdn.net/luoganttcc/article/details/83384823 
+版权声明：本文为博主原创文章，转载请附上博文链接！
