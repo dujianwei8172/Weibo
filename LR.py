5d17ace89b3b08ed259972802ecdef0ceb46b10f
Mashiro <mashiro@MashirotekiMacBook-Air.local>
2019-07-08 10:09:25 +0800

    1

---
+++ LR.py
@@ -0,0 +1,262 @@
+class LogisticRegression():
+    def __init__(self, n_in, n_out):
+        self.X = tf.placeholder(tf.float32, [None, n_in], name='X')
+        self.y = tf.placeholder(tf.int32, [None], name='y')
+        self.init_variables(n_in, n_out)
+        
+    def init_variables(self, n_in, n_out):
+        # n_in means n_features
+        # n_out means n_classes
+        self.W = tf.Variable(
+            initial_value=tf.constant(0.0, shape=[n_in, n_out]),
+            dtype=tf.float32, name='weight')
+        self.b = tf.Variable(
+            initial_value=tf.constant(0.0, shape=[n_out]),
+            dtype=tf.float32, name='bias')
+
+    def softmax(self, logits):
+        # softmax = tf.exp(logits) / tf.reduce_sum(tf.exp(logits), axis)
+        return tf.nn.softmax(logits, -1)
+        
+    def negative_log_likelihood(self, y_pred, y):
+    	# Deprecated.
+        prob = self.sigmoid(y_pred)
+        positive_likelihood = tf.log(prob) * y.reshape(-1, 1)
+        negative_likelihood = tf.log(1 - prob) * (1 - y.reshape(-1, 1))
+        log_likelihood = positive_likelihood + negative_likelihood
+        return -tf.reduce_mean(log_likelihood)
+    
+    def get_network(self):
+        hidden = tf.matmul(self.X, self.W) + self.b
+        self.y_pred = tf.argmax(self.softmax(hidden), axis=-1)  
+        return self.y_pred, hidden
+    
+    def get_loss(self, hidden):
+        # self.loss = self.negative_log_likelihood(y_pred, y)
+        # self.loss = tf.nn.sigmoid_cross_entropy_with_logits(pred, y)
+        onehot_labels = tf.one_hot(self.y, depth=2)
+        self.loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=hidden)
+        return tf.reduce_mean(self.loss)
+    
+    def gen_input(self, data_x, data_y=None):
+        feed_dict = {}
+        feed_dict[self.X] = data_x
+        if data_y is not None:    
+            self.y_truth = data_y
+            feed_dict[self.y] = data_y
+        return feed_dict
+    
+    def errors(self, y_pred, y_truth=None):
+        if y_truth is None:
+            y_truth = self.y
+            not_equal_counts = tf.abs(y_pred - y_truth)
+            return tf.reduce_mean(not_equal_counts)
+        else:
+            not_equal_counts = abs(y_pred - y_truth)
+            return np.mean(not_equal_counts)
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+Define optimizer
+因为深度学习常见的是对于梯度的优化，也就是说，
+优化器最后其实就是各种对于梯度下降算法的优化。
+
+常见的优化器有 SGD，RMSprop，Adagrad，Adadelta，Adam 等，
+此处实例中使用的是随机梯度下降（Stochastic gradient descent），
+因为大多数机器学习任务就是最小化损失，在损失定义的情况下，后面的工作就交给优化器处理即可
+
+def sgd_optimization(datasets, learning_rate=0.10, n_epochs=50, draw_freq=10):
+    train_set_x, train_set_y = datasets[0]
+    test_set_x,  test_set_y  = datasets[1]
+    classifier = LogisticRegression(n_in=2, n_out=2)  # Classifier
+    
+    def get_model_train():
+        with tf.name_scope('train'):
+            y_pred, hidden = classifier.get_network()
+            loss = classifier.get_loss(hidden)
+            return y_pred, loss
+    
+    def get_model_test():
+        with tf.name_scope('test'):
+            y_pred, hidden = classifier.get_network()
+            return y_pred
+        
+    train_output = get_model_train()  # y_pred, loss
+    test_output = get_model_test()  # y_pred
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(train_output[-1])
+    
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+    
+    def call_model(data_x, data_y=None, name=None):
+        # generate data_y for placeholder while testing
+        if data_y is None:
+            data_y = np.zeros(data_x.shape[:-1])
+            
+        if name == 'test':
+            ret = sess.run(  # return y_pred
+                test_output,
+                feed_dict=classifier.gen_input(data_x, data_y))
+        else:  # name == 'train'
+            _, ret = sess.run(  # return y_pred, loss
+                [optimizer, train_output], 
+                feed_dict=classifier.gen_input(data_x, data_y))
+        return ret
+    
+    epoch = 0
+    while epoch < n_epochs:
+        # draw a figure every 'draw_freq' times
+        if epoch % draw_freq == 0:
+            # print(train_set_x, train_set_y)
+            plot_decision_boundary(
+                lambda x: call_model(x)[0], 
+                train_set_x, train_set_y)
+        
+        # print error/cost per epoch
+        train_pred, loss = call_model(
+            train_set_x, train_set_y, 'train')
+        train_error = classifier.errors(
+            y_pred=train_pred, y_truth=train_set_y)
+        
+        test_pred = call_model(
+            test_set_x,  test_set_y, 'test')
+        test_error = classifier.errors(
+            y_pred=test_pred, y_truth=test_set_y)
+
+        print ("epoch is %d, train error %f, test error %f" % (
+            epoch, train_error, test_error))
+        epoch += 1
+
+    # draw a figure at last        
+    plot_decision_boundary(
+        lambda x: call_model(x)[0], 
+        train_set_x, train_set_y)
+    sess.close()
+
+
+def plot_decision_boundary(pred_func, train_set_x, train_set_y):
+    # Draw figures as Matlab 
+    x_min, x_max = train_set_x[:, 0].min() - .5, train_set_x[:, 0].max() + .5
+    y_min, y_max = train_set_x[:, 1].min() - .5, train_set_x[:, 1].max() + .5
+    h = 0.01
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    grid_input = np.c_[xx.ravel(), yy.ravel()].reshape([-1, 2])
+    Z = pred_func(grid_input)
+    plt.contourf(xx, yy, Z.reshape(xx.shape), cmap=plt.cm.Spectral)
+    plt.scatter(train_set_x[:, 0], train_set_x[:, 1], c=train_set_y, cmap=plt.cm.Spectral)
+    plt.show()
+
+
+if __name__=="__main__":
+    data = load_data('moons')
+    sgd_optimization(data, learning_rate=0.1, n_epochs=200, draw_freq=25)
